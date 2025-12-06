023cac1d5d9741dd827d27af8ea8f59a4241529b
Bryan Lown <bryan.lown@gmail.com>
2025-12-06 01:14:37 +0000

    feat(screens): add HomeScreen with basic layout

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+// src/screens/HomeScreen.tsx
+
+import { ScreenWrapper } from '../components/ScreenWrapper';
+import { Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <ScreenWrapper>
+      <Text>Home Screen</Text>
+    </ScreenWrapper>
+  );
+}
