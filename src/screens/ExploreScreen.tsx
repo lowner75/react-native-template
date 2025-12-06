95dc586e9aebc4bca101344ca9d9c654f4a8dc3f
Bryan Lown <bryan.lown@gmail.com>
2025-12-06 01:14:37 +0000

    feat(screens): add ExploreScreen with basic layout

---
+++ src/screens/ExploreScreen.tsx
@@ -0,0 +1,12 @@
+// src/screens/ExploreScreen.tsx
+
+import { ScreenWrapper } from '../components/ScreenWrapper';
+import { Text } from 'react-native';
+
+export default function ExploreScreen() {
+  return (
+    <ScreenWrapper>
+      <Text>Explore Screen</Text>
+    </ScreenWrapper>
+  );
+}
