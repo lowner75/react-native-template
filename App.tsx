903adbd0d11d93a8c497f8f1e3cff8f8c032a473
Bryan Lown <bryan.lown@gmail.com>
2025-12-06 00:39:17 +0000

    feat(app): add initial App.tsx with SafeAreaProvider and basic layout

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
+// App.tsx
+
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app Bry!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
