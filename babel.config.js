79fcb67082836d979b0be4973a640f5197a61dd5
Bryan Lown <bryan.lown@gmail.com>
2025-12-08 09:27:39 +0000

    chore: configure Babel for react-native-dotenv support with @env imports

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+// babel.config.js
+
+export default function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
+  };
+};
